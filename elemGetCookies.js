c27243758d916d84cc8e6c9723aa274da93a7d9a
cy <yechongyuan@hotmail.com>
2022-09-06 09:27:55 +0800

    Create elemGetCookies.js

---
+++ elemGetCookies.js
@@ -0,0 +1,76 @@
+const CookieName = '饿了么'
+const CookieKey = 'cookie_elem'
+const UserId='user_id_elem'
+const matchid=/USERID=(\d+);/
+const sy = init()
+GetCookie();
+
+function GetCookie() {
+  if ($request.headers) {
+    var CookieValue = $request.headers['Cookie'];
+    
+    if (sy.getdata(CookieKey) != (undefined || null)) {
+      if (sy.getdata(CookieKey) != CookieValue) {
+        var cookie = sy.setdata(CookieValue, CookieKey);
+        if (!cookie) {
+          sy.msg("更新" + CookieName + "Cookie失败‼️", "", "");
+        } else {
+          sy.msg("更新" + CookieName + "Cookie成功 🎉", "", "");
+        }
+      }
+    } else {
+      var cookie = sy.setdata(CookieValue, CookieKey);
+      if (!cookie) {
+        sy.msg("首次写入" + CookieName + "Cookie失败‼️", "", "");
+      } else {
+        sy.msg("首次写入" + CookieName + "Cookie成功 🎉", "", "");
+      }
+    }
+  } else {
+    sy.msg("写入" + CookieName + "Cookie失败‼️", "", "配置错误, 无法读取请求头, ");
+  }
+}
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+sy.done()
